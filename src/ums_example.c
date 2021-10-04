649e6963dd20305a65b27c614389b793760f99c2
UmarbaevB <hrafnulf13@gmail.com>
2021-10-04 03:20:46 +0200

    Update example; W/o testing

--- src/ums_example.c
+++ src/ums_example.c
@@ -5,16 +5,40 @@
 void loop()
 {
     printf("UMS_EXAMPLE: %s\n", __FUNCTION__);
+    ums_execute_thread(1);
+    ums_execute_thread(2);
+    ums_execute_thread(1);
+
     ums_exit_scheduling_mode();
 }
 
 void function1(void *args)
 {
-
+    printf("UMS_EXAMPLE: %s\n", __FUNCTION__);
+    printf("UMS_EXAMPLE: args = %d\n", *args);
+    for(int i = 0; i < 5; ++i)
+    {
+        printf("UMS_EXAMPLE: %s => Loop: i = %d\n", __FUNCTION__, i);
+    }
+    ums_thread_pause();
+    printf("UMS_EXAMPLE: Continue: %s\n", __FUNCTION__);
+    printf("UMS_EXAMPLE: args = %d\n", *args);
+    for(int i = 0; i < 5; ++i)
+    {
+        printf("UMS_EXAMPLE: %s => Loop: i = %d\n", __FUNCTION__, i);
+    }
+    ums_thread_exit();
 }
+
 void function2(void *args)
 {
-
+    printf("UMS_EXAMPLE: %s\n", __FUNCTION__);
+    printf("UMS_EXAMPLE: args = %d\n", *args);
+    for(int i = 0; i < 5; ++i)
+    {
+        printf("UMS_EXAMPLE: %s => Loop: i = %d\n", __FUNCTION__, i);
+    }
+    ums_thread_exit();
 }
 
 int main()
@@ -28,11 +52,11 @@ int main()
     int arg1 = 1;
     int arg2 = 2;
     ums_wid_t worker1 = ums_create_worker_thread(comp_list1, STACK_SIZE, function1, &arg1);
-    ums_wid_t worker2 = ums_create_worker_thread(comp_list2, STACK_SIZE, function2, &arg2);
+    ums_wid_t worker2 = ums_create_worker_thread(comp_list1, STACK_SIZE, function2, &arg2);
 
     
     ums_sid_t scheduler1 = ums_create_scheduler(comp_list1, loop);
-    ums_sid_t scheduler2 = ums_create_scheduler(comp_list2, loop);
+    //ums_sid_t scheduler2 = ums_create_scheduler(comp_list2, loop);
     
     ums_exit();
 
